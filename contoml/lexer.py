e4f11e01af0cd5a3e3aa92f36ff8a2e381b89c7e
Amr Hassan <amr.hassan@gmail.com>
2015-06-08 15:28:06 +0200

    lexer: Strings, ints and floats.

---
+++ contoml/lexer.py
@@ -0,0 +1,45 @@
+
+import re
+
+# Priority is encoded as the first two characters of each token type. The lower the sort order, the
+# higher the priority.
+TOKEN_TYPE_STRING = '99-string'
+TOKEN_TYPE_MULTILINE_STRING = '99-multiline_string'
+TOKEN_TYPE_LITERAL_STRING = '99-literal_string'
+TOKEN_TYPE_MULTILINE_LITERAL_STRING = '99-multiline_literal_string'
+TOKEN_TYPE_WHITESPACE = '99-whitespace'
+TOKEN_TYPE_INTEGER = '00-int'
+TOKEN_TYPE_FLOAT = '01-float'
+
+# Pairs of TOKEN_TYPE and Token spec in RegExp.
+_LEXICAL_SPECS = (
+    (TOKEN_TYPE_STRING, re.compile(r'^("(([^"]|\\")+?[^\\]|)")')),                       # Single line only
+    (TOKEN_TYPE_MULTILINE_STRING, re.compile(r'^(""".*?""")', re.DOTALL)),
+    (TOKEN_TYPE_LITERAL_STRING, re.compile(r"^('.*?')")),
+    (TOKEN_TYPE_MULTILINE_LITERAL_STRING, re.compile(r"^('''.*?''')", re.DOTALL)),
+    (TOKEN_TYPE_WHITESPACE, re.compile('^(\s+)', re.DOTALL)),                       # Can span multiple lines
+    (TOKEN_TYPE_INTEGER, re.compile(r'^(((\+|-)[0-9_]+)|([1-9][0-9_]*))')),
+    (TOKEN_TYPE_FLOAT, re.compile(r'^((((\+|-)[0-9_]+)|([1-9][0-9_]*))*(.[0-9_]+)?([eE](\+|-)?[1-9_]+)?)')),
+)
+
+def consume_token(source):
+    """
+    Returns a (TOKEN_TYPE, matched_text) pair if could match a token at the beginning of the
+    given source text, or None if no token type could be matched.
+    """
+    matches = []
+
+    for token_type, token_spec in _LEXICAL_SPECS:
+        match = token_spec.search(source)
+        if match:
+            matches.append((token_type, match.group(1)))
+
+    if len(matches) == 1:
+        return matches[0]
+
+    elif len(matches) > 1:
+        # Return the maximal-munch
+        maximal_munch_length = max(len(matched_text) for _, matched_text in matches)
+        maximal_munches = [match for match in matches if len(match[1]) == maximal_munch_length]
+
+        return sorted(maximal_munches, key=lambda x: x[0])[0]   # Return the first in sorting by priority
---
+++ contoml/test_lexer.py
@@ -0,0 +1,77 @@
+
+from contoml.lexer import *
+
+# A mapping from token types to a sequence of pairs of (source_text, expected_matched_text)
+valid_tokens = {
+    TOKEN_TYPE_STRING: (
+        ('"a valid hug3 text" "some other string" = 42', '"a valid hug3 text"'),
+        (
+            r'"I\'m a string. \"You can quote me\". Name\tJos\u00E9\nLocation\tSF." "some other string" = 42',
+            r'"I\'m a string. \"You can quote me\". Name\tJos\u00E9\nLocation\tSF."'
+        ),
+    ),
+    TOKEN_TYPE_MULTILINE_STRING: (
+        ('"""\nRoses are red\nViolets are blue""" """other text"""', '"""\nRoses are red\nViolets are blue"""'),
+    ),
+    TOKEN_TYPE_LITERAL_STRING: (
+        (r"'This is \ \n a \\ literal string' 'another \ literal string'", r"'This is \ \n a \\ literal string'"),
+    ),
+    TOKEN_TYPE_MULTILINE_LITERAL_STRING: (
+        (
+            "'''\nThe first newline is\ntrimmed in raw strings.\n   All other whitespace\n   is preserved.\n''' '''some other\n\n\t string'''",
+            "'''\nThe first newline is\ntrimmed in raw strings.\n   All other whitespace\n   is preserved.\n'''"
+        ),
+    ),
+    TOKEN_TYPE_WHITESPACE: (
+        (' \t\n \r  some_text', ' \t\n \r  '),
+    ),
+    TOKEN_TYPE_INTEGER: (
+        ('+99 "number"', "+99"),
+        ('42 fwfwef', "42"),
+        ('-17 fh34g34g', "-17"),
+        ('5_349_221 apples', "5_349_221"),
+        ('-1_2_3_4_5 steps', '-1_2_3_4_5')
+    ),
+    TOKEN_TYPE_FLOAT: (
+        ('1.0 fwef', '1.0'),
+        ('3.1415 g4g', '3.1415'),
+        ('-0.01 433re', '-0.01'),
+        ('5e+2_2ersdvf', '5e+2_2'),
+        ('1e6ewe23', '1e6'),
+        ('-2E-2.23rf23', '-2E-2'),
+        ('6.626e-34+234f', '6.626e-34'),
+        ('9_224_617.445_991_228_313f1ewer 23f4h = nonesense', '9_224_617.445_991_228_313'),
+        ('1e1_000 2346f,ef2!!', '1e1_000'),
+    )
+}
+
+# A mapping from a token type to a sequence of (source, matched_text) pairs that shouldn't result from consuming the
+# source text.
+invalid_tokens = {
+    TOKEN_TYPE_INTEGER: (
+        ('_234_423', ''),
+        ('0446234234', ''),
+    ),
+    TOKEN_TYPE_STRING: (
+        ('"""', '"""'),
+    ),
+}
+
+def test_valid_tokenizing():
+    for token_type in valid_tokens:
+        for (source, expected_match) in valid_tokens[token_type]:
+
+            token_data = consume_token(source)
+            assert token_data, "Failed to tokenize: {}\nExpected: {}\nOut of: {}\nGot nothing!".format(token_type, expected_match, source)
+
+            recognized_type, matched_text = token_data
+            assert recognized_type == token_type
+            assert matched_text == expected_match
+
+
+def test_invalid_tokenizing():
+    for token_type in invalid_tokens:
+        for source, expected_match in invalid_tokens[token_type]:
+            token_data = consume_token(source)
+            if token_data:
+                assert token_data[0] != token_type or token_data[1] != expected_match
