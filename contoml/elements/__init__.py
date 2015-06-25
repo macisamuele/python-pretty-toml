24adbc87cdf5468f084444528d4f296a0ec34e56
Amr Hassan <amr.hassan@gmail.com>
2015-06-25 14:27:50 +0200

    Fixed Python3 ImportError

--- contoml/elements/__init__.py
+++ contoml/elements/__init__.py
@@ -2,4 +2,4 @@
 from .traversal import TraversalMixin
 from .errors import InvalidElementError
 
-from common import TYPE_METADATA, TYPE_ATOMIC, TYPE_CONTAINER, TYPE_MARKUP
+from .common import TYPE_METADATA, TYPE_ATOMIC, TYPE_CONTAINER, TYPE_MARKUP
