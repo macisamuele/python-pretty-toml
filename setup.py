1b1422180670509cd3d89dc754bf84ce519ab8ea
Amr Hassan <amr.hassan@gmail.com>
2015-06-30 17:49:40 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='contoml',
     packages=['contoml'],
     version='0.1',
-    description='Consistent TOML for Pythong',
+    description='Consistent TOML for Python',
     author='Amr Hassan',
     author_email='amr.hassan@gmail.com',
     url='https://github.com/Jumpscale/python-consistent-toml',
