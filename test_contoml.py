cc495c264ba05552052261a9c4936f3be6af962d
Amr Hassan <amr.hassan@gmail.com>
2015-07-05 11:49:19 +0200

    Reduced vertical whitespce in test_contoml.py

--- test_contoml.py
+++ test_contoml.py
@@ -3,13 +3,9 @@ import json
 import contoml
 
 def test_it_should_correctly_load_sample_file():
-
     toml_file = contoml.load('dateless_sample.toml')
-
     assert set(toml_file.keys()) == {'fruit', 'clients', '', 'owner', 'database', 'servers', 'extra'}
-
     json_file = json.load(open('dateless_sample.json'))
-
     assert json_file == toml_file.primitive
 
 
