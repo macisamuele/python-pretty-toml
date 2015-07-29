c4c9a661b1455e2864a946a12667ceeb7f663cc5
Amr Hassan <amr.hassan@gmail.com>
2015-07-29 14:19:01 +0200

    Revert "Python 3.5"
    This reverts commit 6f63c4e20190e0c7f636d4b511b5d36715fd3069.

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4'
-        'Programming Language :: Python :: 3.5'
     ],
     install_requires=[
         'six',
