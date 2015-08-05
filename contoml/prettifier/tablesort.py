55fb5cc4bcf28ecc7f6a4155a79dfda565fb0d02
Amr Hassan <amr.hassan@gmail.com>
2015-08-05 14:43:43 +0200

    prettifiers: Fixed crashing on empty lines

--- contoml/prettifier/tablesort.py
+++ contoml/prettifier/tablesort.py
@@ -16,20 +16,18 @@ def __do_table(table):
     def line_ranges():
         # Returns [start, stop) indices of lines in the table
         next_start = 0
-        while True:
-            next_end = t.find_following(elements, t.predicates.newline, next_start)
-            if next_end > next_start:
-                yield (next_start, next_end+1)
-                next_start = next_end + 1
-            else:
-                break
+        while t.find_following(elements, t.predicates.newline, next_start-1) >= 0:
+            next_stop = t.find_following(elements, t.predicates.newline, next_start-1)
+            yield (next_start, next_stop+1)
+            next_start = next_stop + 1
 
     def line_key(line_start, line_stop):
         # Returns the key to sort the lines by
         i = t.find_following(elements, t.predicates.non_metadata, line_start-1)
-        assert(i >= 0)
-        assert(i < line_stop)
-        return elements[i].value
+        if -1 < i < line_stop:
+            return elements[i].value
+        else:
+            return 'z'*20   # Empty line should be at the end
 
     old_elements = table.elements[:]
     sorted_ranges = sorted(tuple(line_ranges()), key=lambda r: line_key(*r))
--- contoml/test_prettifier.py
+++ contoml/test_prettifier.py
@@ -1,3 +1,7 @@
+import contoml
 
 
-
+def test_works():
+    f = contoml.load('sample.toml')
+    f.prettify()
+    f.dump('sample-prettified.toml')
