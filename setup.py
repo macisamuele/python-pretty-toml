d2013474c1ae75cf5b12dfd185b3c9c56a320f34
Amr Hassan <amr.hassan@gmail.com>
2015-10-12 16:47:25 +0200

    Officialy supporting Python 3.5

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4'
+        'Programming Language :: Python :: 3.5'
     ],
     install_requires=[
         'six',
