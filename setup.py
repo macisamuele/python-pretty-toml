bbcd67390bfa620dd52e2112f5dc1a91b27e0223
Samuele Maci <macisamuele@gmail.com>
2018-07-29 13:48:52 +0200

    Update setup.py and travis to expose python 3.6 and python3.7 compatibility

--- setup.py
+++ setup.py
@@ -11,9 +11,13 @@ setup(
     url='https://github.com/Jumpscale/python-prettytoml',
     keywords=['toml'],
     classifiers=[
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5'
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     install_requires=[
         'six',
